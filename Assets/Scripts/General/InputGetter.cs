3cbb8f7c9e304e95eb15e68d590411ce545fb90f
foriver4725 <hrt67@icloud.com>
2024-05-27 17:11:17 +0900

    Revert "タイトルのインプット試作"
    This reverts commit 7f487d9015ca8300441bffc151f138163c5bb1ac.

--- Assets/Scripts/General/InputGetter.cs
+++ Assets/Scripts/General/InputGetter.cs
@@ -45,18 +45,18 @@ namespace IA
         #endregion
 
         #region 変数宣言
-        public bool Title_IsStart { get; private set; } = false;
-        public bool Title_IsQuit { get; private set; } = false;
-        //public Vector2 ValueMove { get; private set; } = Vector2.zero;
-        //public bool IsHold { get; private set; } = false;
+        public bool IsSubmit { get; private set; } = false;
+        public bool IsCancel {  get; private set; } = false;
+        public Vector2 ValueMove { get; private set; } = Vector2.zero;
+        public bool IsHold { get; private set; } = false;
         #endregion
 
         #region【LateUpdate】毎フレームの最後で、フラグを初期化する
         void LateUpdate()
         {
-            if (Title_IsStart) Title_IsStart = false;
-            if (Title_IsQuit) Title_IsQuit = false;
-            //if (IsHold) IsHold = false;
+            if (IsSubmit) IsSubmit = false;
+            if (IsCancel) IsCancel = false;
+            if (IsHold) IsHold = false;
         }
         #endregion
 
@@ -66,51 +66,51 @@ namespace IA
             // インスタンス名.Map名.Action名.コールバック名
             if (isLink)
             {
-                _inputs.Title.Start.performed += Title_OnStart;
+                _inputs.Test.Submit.performed += OnSubmit;
 
-                _inputs.Title.Quit.performed += Title_OnQuit;
+                _inputs.Test.Cancel.performed += OnCancel;
 
-                //_inputs.Test.Move.started += ReadMove;
-                //_inputs.Test.Move.performed += ReadMove;
-                //_inputs.Test.Move.canceled += ReadMove;
+                _inputs.Test.Move.started += ReadMove;
+                _inputs.Test.Move.performed += ReadMove;
+                _inputs.Test.Move.canceled += ReadMove;
 
-                //_inputs.Test.Hold.performed += OnHold;
+                _inputs.Test.Hold.performed += OnHold;
             }
             else
             {
-                _inputs.Title.Start.performed -= Title_OnStart;
+                _inputs.Test.Submit.performed -= OnSubmit;
 
-                _inputs.Title.Quit.performed -= Title_OnQuit;
+                _inputs.Test.Cancel.performed -= OnCancel;
 
-                //_inputs.Test.Move.started -= ReadMove;
-                //_inputs.Test.Move.performed -= ReadMove;
-                //_inputs.Test.Move.canceled -= ReadMove;
+                _inputs.Test.Move.started -= ReadMove;
+                _inputs.Test.Move.performed -= ReadMove;
+                _inputs.Test.Move.canceled -= ReadMove;
 
-                //_inputs.Test.Hold.performed -= OnHold;
+                _inputs.Test.Hold.performed -= OnHold;
             }
         }
         #endregion
 
         #region 処理の詳細
-        void Title_OnStart(InputAction.CallbackContext context)
+        void OnSubmit(InputAction.CallbackContext context)
         {
-            Title_IsStart = true;
+            IsSubmit = true;
         }
 
-        void Title_OnQuit(InputAction.CallbackContext context)
+        void OnCancel(InputAction.CallbackContext context)
         {
-            Title_IsQuit = true;
+            IsCancel = true;
         }
 
-        //void ReadMove(InputAction.CallbackContext context)
-        //{
-        //    ValueMove = context.ReadValue<Vector2>();
-        //}
+        void ReadMove(InputAction.CallbackContext context)
+        {
+            ValueMove = context.ReadValue<Vector2>();
+        }
 
-        //void OnHold(InputAction.CallbackContext context)
-        //{
-        //    IsHold = true;
-        //}
+        void OnHold(InputAction.CallbackContext context)
+        {
+            IsHold = true;
+        }
         #endregion
     }
 }
