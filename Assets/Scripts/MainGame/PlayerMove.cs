3f71b53ea1391d79e99b1b39ecbf3d69e5f3366a
foriver4725 <hrt67@icloud.com>
2024-05-27 23:56:57 +0900

    プレイヤーが1マス単位でしか移動できなくし、アニメーションが即座に遷移するようにした。

--- Assets/Scripts/MainGame/PlayerMove.cs
+++ Assets/Scripts/MainGame/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,29 +18,55 @@ namespace MainGame
         enum DIR { DOWN, LEFT, UP, RIGHT }
         DIR lookingDir = DIR.DOWN;
 
-        float speed = 1f;
+        // プレイヤーが整数座標まで移動完了しているか
+        bool IsStepEnded = true;
+
+        float speed = 5f;
 
         void Update()
         {
-            Vector2 inputDir = IA.InputGetter.Instance.MainGame_ValueMove;
-
-            // （動いているなら）向いている方向を判断する。
-            if (inputDir != Vector2.zero)
+            if (IsStepEnded)
             {
-                float dot1 = Vector2.Dot(baseVec1, inputDir);
-                float dot2 = Vector2.Dot(baseVec2, inputDir);
-                if (dot1 >= 0 && dot2 >= 0) lookingDir = DIR.DOWN;
-                else if (dot1 >= 0 && dot2 < 0) lookingDir = DIR.LEFT;
-                else if (dot1 < 0 & dot2 >= 0) lookingDir = DIR.RIGHT;
-                else lookingDir = DIR.UP;
+                Vector2 inputDir = IA.InputGetter.Instance.MainGame_ValueMove;
+
+                // （動いているなら）向いている方向を判断する。
+                if (inputDir != Vector2.zero)
+                {
+                    float dot1 = Vector2.Dot(baseVec1, inputDir);
+                    float dot2 = Vector2.Dot(baseVec2, inputDir);
+                    if (dot1 >= 0 && dot2 >= 0) lookingDir = DIR.DOWN;
+                    else if (dot1 >= 0 && dot2 < 0) lookingDir = DIR.LEFT;
+                    else if (dot1 < 0 & dot2 >= 0) lookingDir = DIR.RIGHT;
+                    else lookingDir = DIR.UP;
+                }
+
+                // アニメーションの遷移を発火させる。
+                anim.SetBool("IsMoving", inputDir != Vector2.zero);
+                anim.SetInteger("LookingDirection", (int)lookingDir);
+
+                // 必ず向いている方向の次の整数座標まで移動し、その間は入力を受け付けない。
+                if (inputDir != Vector2.zero)
+                {
+                    IsStepEnded = false;
+                    StartCoroutine(MoveTo(GetMoveDir(lookingDir)));
+                }
+            }
+        }
+
+        IEnumerator MoveTo(Vector3 dir)
+        {
+            Vector3 fromPos = transform.position;
+            Vector3 toPos = fromPos + dir;
+
+            while (true)
+            {
+                transform.position += speed * Time.deltaTime * dir;
+                if ((transform.position - fromPos).sqrMagnitude >= 1) break;
+                yield return null;
             }
 
-            // アニメーションの遷移を発火させる。
-            anim.SetBool("IsMoving", inputDir != Vector2.zero);
-            anim.SetInteger("LookingDirection", (int)lookingDir);
-
-            // （動いているなら）向いている方向に移動する。
-            if (inputDir != Vector2.zero) transform.position += speed * Time.deltaTime * GetMoveDir(lookingDir);
+            transform.position = toPos;
+            IsStepEnded = true;
         }
 
         // プレイヤーの向きから、その方向の単位ベクトルを返す。
