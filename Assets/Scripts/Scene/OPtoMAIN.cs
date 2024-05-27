112521fa833791d84ae258c735ce590dae4bf027
manato1201 <manato1201m@icloud.com>
2024-05-27 17:21:50 +0900

    タイトル作成ver4（ゴミ）

--- Assets/Scripts/Scene/OPtoMAIN.cs
+++ Assets/Scripts/Scene/OPtoMAIN.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OPtoMAIN : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class OPtoMAIN : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if (IA.InputGetter.Instance.)
+        //{
+        //    SceneManager.LoadSceneAsync("Main");
+        //}
     }
 }
