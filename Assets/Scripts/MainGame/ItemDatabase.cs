966cb968462cadcbd995d81892ea057e57e41208
TK230378 畑山郁弥 <tk230378@tks.iput.ac.jp>
2024-07-01 14:57:06 +0900

    add "ItemDatabase"

---
+++ Assets/Scripts/MainGame/ItemDatabase.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+namespace MainGame
+{
+    public static class ItemDatabase
+    {
+        // Dictionary to hold item names and their quantities
+        public static List<string> Items = new List<string>();
+
+        // Method to add an item or increase its quantity
+        public static void GetItem(string itemName)
+        {
+            Items.Add(itemName);
+        }
+
+        public static bool CheckItem(string itemName)
+        {
+            return Items.Contains(itemName);
+        }
+    }
+
+}
