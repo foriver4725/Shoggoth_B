5dfbbcb728b7b337f99eb4bd61771352668e86aa
Morisawaa <tk230108@tks.iput.ac.jp>
2024-07-01 17:23:42 +0900

    クリア画面の修正、5秒以降後にボタンを押すことでOPシーンに移動

--- Assets/Scripts/Scene/CLEARtoOP.cs
+++ Assets/Scripts/Scene/CLEARtoOP.cs
@@ -8,20 +8,27 @@ namespace Scene
 {
     public class CLEARtoOP : MonoBehaviour
     {
+        private bool _isClickable = false;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            StartCoroutine(CountClickTime());
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (IA.InputGetter.Instance.System_IsSubmit)
+            if (_isClickable && IA.InputGetter.Instance.System_IsSubmit)
             {
-                Debug.Log(1);
                 SceneManager.LoadSceneAsync(SO_SceneName.Entity.Title);
             }
+        }
+
+        IEnumerator CountClickTime()
+        {
+            yield return new WaitForSeconds(5);
+            _isClickable = true;
         }
     }
 }
