f1f7d41ff812fa5f4f49246edf1e2b7c1a88f066
manato1201 <manato1201m@icloud.com>
2024-07-16 15:40:36 +0900

    ログ表示1

--- Assets/Scripts/MainGame/UITextManager.cs
+++ Assets/Scripts/MainGame/UITextManager.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using SO;
+using IA;
+using TMPro;
+using static UnityEngine.Mesh;
 
 public class UITextManager : MonoBehaviour
 {
-
+    public TextMeshProUGUI textMeshProUGUI;
+    public SO_UIConsoleText uiConsoleText;
+    int i = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiConsoleText = Resources.Load<SO_UIConsoleText>("SO_UIConsoleText");
     }
+
 
     // Update is called once per frame
     void Update()
     {
+       
+            textMeshProUGUI.text=SO_UIConsoleText.Entity.ShoggothLog.ToString();
+            
+        
         
     }
 }
