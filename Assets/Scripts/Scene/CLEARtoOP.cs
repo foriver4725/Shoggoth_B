f0c63454b5d427a03e5d595fbff24db26d67c6ae
foriver4725 <hrt67@icloud.com>
2024-10-21 17:48:09 +0900

    feature: エンディングムービーを再生するようにした

--- Assets/Scripts/Scene/CLEARtoOP.cs
+++ Assets/Scripts/Scene/CLEARtoOP.cs
@@ -1,51 +1,29 @@
 using Cysharp.Threading.Tasks;
-using Ex;
 using SO;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 namespace Scene
 {
-    public class CLEARtoOP : MonoBehaviour
+    public sealed class CLEARtoOP : MonoBehaviour
     {
-        [SerializeField] private GameObject _endDescription;
-        [SerializeField] private TextMeshProUGUI _nextText;
-        [SerializeField] private TextMeshProUGUI _backText;
-        [SerializeField] private AudioSource _click1st;
-        [SerializeField] private AudioSource _click2nd;
-
-        private CancellationToken _ct;
+        [SerializeField] private VideoPlayer videoPlayer;
 
         void Start()
         {
-            _ct = this.GetCancellationTokenOnDestroy();
-            SceneChange(_ct).Forget();
+            videoPlayer.loopPointReached += OnPlayEnded;
+            videoPlayer.Play();
         }
 
-        private async UniTask SceneChange(CancellationToken ct)
+        private void OnPlayEnded(VideoPlayer _) => SceneChange(destroyCancellationToken).Forget();
+
+        private async UniTaskVoid SceneChange(CancellationToken ct)
         {
-            _endDescription.SetActive(true);
-            _nextText.color = Color.black;
-            _backText.color = Color.black;
-
-            await UniTask.Delay(TimeSpan.FromSeconds(SO_General.Entity.ClickDur), cancellationToken: ct);
-            await UniTask.WaitUntil(() => IA.InputGetter.Instance.System_IsSubmit, cancellationToken: ct);
-            _nextText.color = Color.yellow;
-            _click1st.Raise(SO_Sound.Entity.ClickSE, SType.SE);
-            await UniTask.Delay(TimeSpan.FromSeconds(SO_General.Entity.AfterClickDur), cancellationToken: ct);
-            _endDescription.SetActive(false);
-
-            await UniTask.Delay(TimeSpan.FromSeconds(SO_General.Entity.ClickDur), cancellationToken: ct);
-            await UniTask.WaitUntil(() => IA.InputGetter.Instance.System_IsSubmit, cancellationToken: ct);
-            _backText.color = Color.yellow;
-            _click2nd.Raise(SO_Sound.Entity.ClickSE, SType.SE);
-            await UniTask.Delay(TimeSpan.FromSeconds(SO_General.Entity.AfterClickDur), cancellationToken: ct);
-            await SceneManager.LoadSceneAsync(SO_SceneName.Entity.Title);
+            await UniTask.Delay(TimeSpan.FromSeconds(3), cancellationToken: ct);
+            SceneManager.LoadScene(SO_SceneName.Entity.Title);
         }
     }
 }
