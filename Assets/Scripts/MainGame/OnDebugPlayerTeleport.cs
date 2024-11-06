6507f5b8d533bc86a4000c69a361c7bbd7895469
foriver4725 <hrt67@icloud.com>
2024-11-06 14:48:04 +0900

    feature: プレイヤーのテレポート、被弾エフェクト

--- Assets/Scripts/MainGame/OnDebugPlayerTeleport.cs
+++ Assets/Scripts/MainGame/OnDebugPlayerTeleport.cs
@@ -7,11 +7,10 @@ namespace MainGame
     {
         private void Update()
         {
-            if (InputGetter.Instance.DebugAction1.Bool) transform.position = new Vector3(0, 0, 0);
-            else if (InputGetter.Instance.DebugAction2.Bool) transform.position = new Vector3(0, 0, 0);
-            else if (InputGetter.Instance.DebugAction3.Bool) transform.position = new Vector3(0, 0, 0);
-            else if (InputGetter.Instance.DebugAction4.Bool) transform.position = new Vector3(0, 0, 0);
-            else if (InputGetter.Instance.DebugAction5.Bool) transform.position = new Vector3(0, 0, 0);
+            if (InputGetter.Instance.DebugAction1.Bool) transform.position = new Vector3(9, 106, -1);
+            else if (InputGetter.Instance.DebugAction2.Bool) transform.position = new Vector3(35, 125, -1);
+            else if (InputGetter.Instance.DebugAction3.Bool) transform.position = new Vector3(109, 11, -1);
+            else if (InputGetter.Instance.DebugAction4.Bool) transform.position = new Vector3(126, 30, -1);
         }
     }
 }
