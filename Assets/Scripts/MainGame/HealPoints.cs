7e496f33d23eca240f9d2b270bc10ea06cef2580
foriver4725 <hrt67@icloud.com>
2024-11-18 13:41:43 +0900

    feature: 回復自販機のきらきら追加、入手したら非表示に

--- Assets/Scripts/MainGame/HealPoints.cs
+++ Assets/Scripts/MainGame/HealPoints.cs
@@ -11,9 +11,13 @@ namespace MainGame
         [SerializeField, Required, SceneObjectsOnly]
         private List<Transform> healPoints;
 
+        [SerializeField, Required, SceneObjectsOnly, Tooltip("↑と一致させること！")]
+        private List<SpriteRenderer> kirakiras;
+
         public bool IsInteractableAgainstAny(PlayerMove playerMove, bool isRemoveFromList = true)
         {
             if (healPoints is null) return false;
+            if (kirakiras is null) return false;
             if (playerMove == null) return false;
 
             foreach (Transform e in healPoints)
@@ -21,7 +25,16 @@ namespace MainGame
                 if (e == null) continue;
                 if (playerMove.IsInteractableAgainst(e.position))
                 {
-                    if (isRemoveFromList) healPoints.Remove(e);
+                    int i = healPoints.IndexOf(e);
+                    SpriteRenderer k = kirakiras[i];
+                    if (k != null) k.transform.position = new(-100, -100, k.transform.position.z);
+
+                    if (isRemoveFromList)
+                    {
+                        healPoints.Remove(e);
+                        kirakiras.Remove(k);
+                    }
+
                     return true;
                 }
             }
